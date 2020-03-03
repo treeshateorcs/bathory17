df33681bf5b3cfb1b47e2544fe4851e53d636219
тно <basedtho@gmail.com>
2020-03-03 09:57:30 +0300

    print feed titles

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/SlyMarbo/rss"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	feed, err := rss.Fetch("https://github.com/qutebrowser/qutebrowser/releases.atom")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range feed.Items {
+		fmt.Println(f.Title)
+	}
 }
